b68f5a6319433b272bdc5c306bb05b7bfa33ce47
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-25 06:03:18 -0500

    added calendar widget and date and time functionality from moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,38 +18,38 @@ $(".card .list-group").sortable({
     console.log("out", event.target);
   },
   update: function(event) {
-// array to store the task data in
-var tempArr = [];
+      // array to store the task data in
+      var tempArr = [];
 
-// loop over current set of children in sortable list
-$(this).children().each(function() {
-  var text = $(this)
-    .find("p")
-    .text()
-    .trim();
+      // loop over current set of children in sortable list
+      $(this).children().each(function() {
+        var text = $(this)
+          .find("p")
+          .text()
+          .trim();
 
-  var date = $(this)
-    .find("span")
-    .text()
-    .trim();
+        var date = $(this)
+          .find("span")
+          .text()
+          .trim();
 
-  // add task data to the temp array as an object
-  tempArr.push({
-    text: text,
-    date: date
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date
+        });
+});
+  // trim down list's ID to match object property
+  var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+  // update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+  console.log(tempArr);
+    }
   });
-});
-// trim down list's ID to match object property
-var arrName = $(this)
-  .attr("id")
-  .replace("list-", "");
-
-// update array on tasks object and save
-tasks[arrName] = tempArr;
-saveTasks();
-console.log(tempArr);
-  }
-});
 
 $("#trash").droppable({
   accept: ".card .list-group-item",
@@ -79,6 +79,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -204,12 +206,21 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
+
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this).val();
 
   // get status type and position in the list
@@ -230,6 +241,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+    
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -240,6 +254,33 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+ // get date from task element
+ var date = $(taskEl).find("span").text().trim();
+ // ensure it worked
+ console.log(date); 
+
+ // convert to moment object at 5:00pm
+ var time = moment(date, "L").set("hour", 17);
+ // this should print out an object for the value of the date variable, but at 5:00pm of that date
+ console.log(time);
+ // remove any old classes from element
+ $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+ // apply new class if task is near/over due date
+ if (moment().isAfter(time)) {
+   $(taskEl).addClass("list-group-item-danger");
+ }
+ else if (Math.abs(moment().diff(time, "days")) <= 2) {
+  $(taskEl).addClass("list-group-item-warning");
+}
+};
+
 
 // load tasks for the first time
 loadTasks();
