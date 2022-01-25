9ce66fcc4431c60a622eb31e798675daf8ed1f4a
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-01-25 11:13:54 -0500

    added custom effects on top of bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
     console.log("activate", this);
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
     console.log("deactivate", this);
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     console.log("over", event.target);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     console.log("out", event.target);
   },
   update: function(event) {
@@ -59,10 +65,12 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -129,7 +137,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -258,6 +266,13 @@ $("#remove-tasks").on("click", function() {
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
+
 
 var auditTask = function(taskEl) {
  // get date from task element
